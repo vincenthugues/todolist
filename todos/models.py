37e0dbb14cda2cf5112848f4fc42cd7e87d8996f
Vincent Hugues <vincenthugues.dev@gmail.com>
2017-08-29 02:41:29 +0200

    Added 'done' field to Todo

--- todos/models.py
+++ todos/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Todo(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
+    done = models.BooleanField(default=False, blank=True)
     created_at = models.DateTimeField(default=datetime.now, blank=True)
     def __str__(self):
         return self.title
